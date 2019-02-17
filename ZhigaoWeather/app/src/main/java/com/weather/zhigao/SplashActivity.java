d01787ddd8ca7430b7860ae960e0fa907bd772bf
GL <gl@GaoLeideMacBook-Pro.local>
2019-02-18 01:51:55 +0800

    实现splash页面icon动画

--- ZhigaoWeather/app/src/main/java/com/weather/zhigao/SplashActivity.java
+++ ZhigaoWeather/app/src/main/java/com/weather/zhigao/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.weather.zhigao;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
@@ -12,6 +13,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -42,13 +45,24 @@ import java.util.Map;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView ivSplashIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarUtil.setTransparent(this);
         setContentView(R.layout.activity_splash);
-        setAndroidNativeLightStatusBar(this,true);
+
+        ivSplashIcon = findViewById(R.id.iv_splash_icon);
+
+        ivSplashIcon.post(new Runnable() {
+            @Override
+            public void run() {
+                startAnimation();
+            }
+        });
+
+        setAndroidNativeLightStatusBar(this, true);
         App.addAppActivity(this);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
 
@@ -61,18 +75,20 @@ public class SplashActivity extends AppCompatActivity {
             }, 3000);
 
     }
+
     private static void setAndroidNativeLightStatusBar(Activity activity, boolean dark) {
 
         View decor = activity.getWindow().getDecorView();
-    if (dark) {
-        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-    } else {
-        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (dark) {
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        } else {
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
     }
-}
 
     // 用来计算返回键的点击间隔时间
     private long exitTime = 0;
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK
@@ -89,5 +105,30 @@ public class SplashActivity extends AppCompatActivity {
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+    private void startAnimation() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(ivSplashIcon, "translationY", 0, -(ivSplashIcon.getHeight() >> 1));
+        animator.setDuration(800);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+//        animator.setRepeatMode(ObjectAnimator.RESTART);
+        animator.setInterpolator(new CycleInterpolator(0.5f));
+        animator.start();
+    }
+
+    private void stopAnimation() {
+        ivSplashIcon.clearAnimation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopAnimation();
     }
 }
